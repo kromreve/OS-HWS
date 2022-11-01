1b5834108ce1d33fa6f698853929fc9189c1c094
weberjr1507 <weberjr1507@gmail.com>
2022-11-01 12:41:09 -0500

    compiled HWS.cpp, created an input file

--- HWS.cpp
+++ HWS.cpp
@@ -1,6 +1,32 @@
+#include <stdio.h>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
+string stringLine;
+
+void extractInts(string str) {
+  stringstream ss;
+  ss << str;
+  string tmp;
+  int found;
+  while (!ss.eof()) {
+    ss >> tmp;
+    if (stringstream(tmp) >> found)
+      cout << found << ", ";
+
+    tmp = " ";
+  }
+}
+
 int main(){
-  
+  ifstream IFILE("input.txt");
+
+  while (getline (IFILE, stringLine)) {
+    extractInts(stringLine);
+    return 0;
+  }
+
+
+IFILE.close();
 }
