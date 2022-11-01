c2d812e50d81d1454ff47a02b8bdd247f6a84f5a
natannk06 <105123824+natannk06@users.noreply.github.com>
2022-11-01 18:14:19 -0500

    Update HWS.hpp

--- HWS.hpp
+++ HWS.hpp
@@ -1,1 +1,13 @@
 
+
+typedef struct process{
+		int pid; 			 
+		int burst;
+    int arrival;
+		int priority; 	
+		int deadline;
+    int io;
+} Process;
+
+
+void getLine(std::string line, int pvalues[] );
