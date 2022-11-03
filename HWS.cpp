8aa30ce4aa7ab092d27805a7af93c16902dfe606
weberjr1507 <weberjr1507@gmail.com>
2022-11-03 08:59:39 -0500

    removed int filter

--- HWS.cpp
+++ HWS.cpp
@@ -5,26 +5,11 @@ using namespace std;
 
 string stringLine;
 
-void extractInts(string str) {
-  stringstream ss;
-  ss << str;
-  string tmp;
-  int found;
-  while (!ss.eof()) {
-    ss >> tmp;
-    if (stringstream(tmp) >> found)
-      cout << found << ", ";
-
-    tmp = " ";
-  }
-}
-
 int main(){
   ifstream IFILE("input.txt");
 
   while (getline (IFILE, stringLine)) {
-    extractInts(stringLine);
-    return 0;
+    cout << stringLine;
   }
 
 
