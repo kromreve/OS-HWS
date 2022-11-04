9c224bc14fad8d20ef7c3854b1220355bb972a8e
Vaughn Kromrey <vaughn.kromrey@gmail.com>
2022-11-04 16:48:26 -0500

    Sanitiaztion updates

--- VAUGHN-HWS.cpp
+++ VAUGHN-HWS.cpp
@@ -2,14 +2,20 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <regex>
 using namespace std;
 
 vector<vector<string>> readInput(const string filename){
     string line;
     ifstream file(filename);
     vector<vector<string>> lines;
+
+    regex regexp("[^0-9 \t-]");
+    
     
     while(getline(file,line)){
+        cout << regex_replace(line, regexp, "") << '\n';
+        //string cleanLine = cleanLine.replace("/[^0-9]/g",' ');
         vector<string> row;
         row.push_back(line);
         lines.push_back(row);
@@ -27,7 +33,7 @@ void print(vector<vector<string>> input){
 
 void printBurst(vector<vector<string>> input){
     for(int i = 0; i < input.size(); i++){
-        cout << input[i][1];
+        cout << input[i][i];
     }
 }
 
@@ -35,7 +41,7 @@ int main(int argc,  char **argv){
     string processes=argv[1];
     vector<vector<string>> input = readInput(processes);
     //print(input);
-    cout << input[0][0];
-    printBurst(input);
+    //cout << input[0][0];
+    //printBurst(input);
     return 0;
 }
