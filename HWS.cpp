8ed16902e7fd388f439e747cbf3e30fbf58819b0
natannk06 <105123824+natannk06@users.noreply.github.com>
2022-11-06 10:09:34 -0600

    Update HWS.cpp

--- HWS.cpp
+++ HWS.cpp
@@ -1,32 +1,71 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
+
 using namespace std;
+
 
 string stringLine;
 
-void extractInts(string str) {
-  stringstream ss;
-  ss << str;
-  string tmp;
-  int found;
-  while (!ss.eof()) {
-    ss >> tmp;
-    if (stringstream(tmp) >> found)
-      cout << found << ", ";
+typedef struct process{
+	int pid; 			 
+	int burst;
+    int arrival;
+	int priority; 	
+	int deadline;
+    int io;
+} Process;
 
-    tmp = " ";
-  }
-}
+
 
 int main(){
-  ifstream IFILE("input.txt");
 
-  while (getline (IFILE, stringLine)) {
-    extractInts(stringLine);
-    return 0;
-  }
+	
+	string stringLine;
+  vector<process> allProcess;
+  string fileName = "";
+		cout << "Enter the file name: " << endl;
+		cin >> fileName;
+		
 
+  
+  ifstream IFILE(fileName);
+  int retry = 0;
+  
+  getline(IFILE, stringLine);
+  int pvalues [6];
+  int n =0;
+  
+  while (getline (IFILE, stringLine)){
+
+			
+			 for (int i = 0; i < 6; i++)
+				{
+    				IFILE >> pvalues[i];
+			
+				}
+				
+
+				if (pvalues[0] >= 0 && pvalues[1] >= 0 && pvalues[2] >= 0 && pvalues[3] >= 0 && pvalues[4] >= 0 && pvalues[5] >= 0) { 
+
+						process current;
+						current.pid = pvalues[0];
+						current.burst = pvalues[1];
+						current.arrival = pvalues[2];
+						current.priority = pvalues[3];
+            			current.deadline = pvalues[4];
+            			current.io = pvalues[5];
+
+				
+						
+						allProcess.push_back(current);	
+
+				}	
+
+
+			 
+		
 
 IFILE.close();
 }
