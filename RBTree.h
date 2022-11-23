83335d0cd09b31b817a7c1d59331fcb69a234f4e
natannk06 <105123824+natannk06@users.noreply.github.com>
2022-11-22 23:26:40 -0600

    Update RBTree.h

--- RBTree.h
+++ RBTree.h
@@ -5,33 +5,40 @@ using std::string;
 #define TREE_H
 
 struct Node;
-typedef Node *NodePtr;
+typedef Node* NodePtr;
+
 class RBTree {
-    
+
 private:
 
     NodePtr root;
-    NodePtr TNULL;
+    
+    bool found = false;
+    
 
     void createEmptyNode(NodePtr node, NodePtr parent);
     void preOrderRec(NodePtr node);
     void inOrderRec(NodePtr node);
     void postOrderRec(NodePtr node);
-    void priorityOrderRec(NodePtr node);
-    NodePtr searchTreeRec(NodePtr node, int key);
+    NodePtr priorityOrderRec(NodePtr node);
+    NodePtr searchTreeRec(NodePtr node, int id);
+    NodePtr getLeftmostRec(NodePtr node);
     void delUpdate(NodePtr x);
     void redBlackTransplant(NodePtr u, NodePtr v);
-    void deleteNodeRec(NodePtr node, int key);
+    void deleteNodeRec(NodePtr node);
     void fixInsert(NodePtr k);
     void printRec(NodePtr root, string indent, bool last);
 
 public:
-   
+
+    NodePtr queriedNode = nullptr;
+    NodePtr TNULL;
+
     RBTree();
     void preOrder();
     void inOrder();
     void postOrder();
-    void priorityOrder();
+    NodePtr priorityOrder();
     NodePtr searchTree(int k);
     NodePtr minimum(NodePtr node);
     NodePtr maximum(NodePtr node);
@@ -39,9 +46,12 @@ public:
     NodePtr predecessor(NodePtr x);
     void leftRotate(NodePtr x);
     void rightRotate(NodePtr x);
+    NodePtr getTopNode();
+    int * exportProcess(NodePtr node);
     void insert(int id, int bst, int avl, int pri, int dln, int io);
+    void printProcess(NodePtr node);
     NodePtr getRoot();
-    void deleteNode(int value);
+    void deleteNode(NodePtr node);
     void formatPrint();
 };
 #endif
